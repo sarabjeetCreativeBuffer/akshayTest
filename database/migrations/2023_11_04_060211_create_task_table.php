8889baecdb67a54c70cff417edae1f8db637c1dd
sarabjeetCreativeBuffer <98382327+sarabjeetCreativeBuffer@users.noreply.github.com>
2023-11-04 12:02:18 +0530

    first commit

---
+++ database/migrations/2023_11_04_060211_create_task_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id();
+            $table->string('task_name')->nullable();
+            $table->integer('category_id');
+            $table->string('status',50)->comment('pending/completed');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task');
+        
+    }
+}
